618174da4d393a08c11d020f88c0235d2432505b
yc2o <yeremiaoctaviano25@gmail.com>
2025-05-31 00:48:40 +0700

    feat: implement SavedScanScreen and update app colors and scan data model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:scano/dat/scan_data.dart';
+import 'package:scano/screens/saved_scan_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID');
   runApp(const MainApp());
 }
 
@@ -8,13 +13,20 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      home: SavedScanScreen(scanData: scanDataMock),
+      debugShowCheckedModeBanner: false,
+    );
+    /*
+    const MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
     );
+    */
   }
 }
